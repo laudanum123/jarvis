50c4704bf30398162ab984831a8d17a0989eec87
Yazid <yazeedkurdi@hotmail.com>
2023-03-03 20:36:26 +0300

    keyword activation

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
+import json
 import pyaudio
 import numpy as np
 import logging
 import warnings
+import vosk
 
 # Define parameters
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 44100
+RATE = 16000
 CHUNK = 1024
 THRESHOLD = 50
+
+# Initialize Vosk model
+model = vosk.Model(model_name = "vosk-model-small-en-us-0.15")
+recognizer = vosk.KaldiRecognizer(model, RATE)
 
 # Initialize PyAudio
 p = pyaudio.PyAudio()
 
 # Open audio stream
-stream = p.open(format=FORMAT,
+stream = p.open(format=pyaudio.paInt16,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
@@ -23,18 +29,23 @@ stream = p.open(format=FORMAT,
 while True:
     # Read audio data
     data = stream.read(CHUNK)
+
+    recognizer.AcceptWaveform(data)
+    result = recognizer.Result()
+    text = json.loads(result)['text']
     # Convert audio data to numpy array
-    data = np.frombuffer(data, dtype=np.int16)
+    data_thresh = np.frombuffer(data, dtype=np.int16)
 
     # Capture and log warnings
 
-    square = np.square(data)
+    square = np.square(data_thresh)
     mean = np.mean(square)
     sqrt = np.sqrt(mean)
 
     rms = sqrt
 
     # Check if the root mean square is above the threshold
-    if rms > THRESHOLD:
-        # If sound is detected, do something
-        print("Sound detected!")
+    if rms > THRESHOLD and 'hello' in text:
+    # If sound is detected, use Vosk to transcribe speech
+        # If "Hello" is recognized, do something
+        print("Hello detected!")
