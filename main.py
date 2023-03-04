21f9ba6dede9ca92532281046af758fe2896f751
Yazid <yazeedkurdi@hotmail.com>
2023-03-04 22:30:16 +0300

    code refactor - scraped vosk - used pvporcupine - added wake word jarvis

--- main.py
+++ main.py
@@ -1,51 +1,37 @@
-import json
-import pyaudio
 import numpy as np
-import logging
-import warnings
-import vosk
-
-# Define parameters
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
-CHUNK = 1024
-THRESHOLD = 50
-
-# Initialize Vosk model
-model = vosk.Model(model_name = "vosk-model-small-en-us-0.15")
-recognizer = vosk.KaldiRecognizer(model, RATE)
-
-# Initialize PyAudio
-p = pyaudio.PyAudio()
+from utilities import p,wave_recorder,FORMAT,CHANNELS,RATE,CHUNK,THRESHOLD,wake_word
 
 # Open audio stream
-stream = p.open(format=pyaudio.paInt16,
+stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
                 input=True,
                 frames_per_buffer=CHUNK)
 
-while True:
-    # Read audio data
-    data = stream.read(CHUNK)
+# FOR TESTING PURPOSES - Set up the wave file writer
+wf = wave_recorder()
 
-    recognizer.AcceptWaveform(data)
-    result = recognizer.Result()
-    text = json.loads(result)['text']
-    # Convert audio data to numpy array
-    data_thresh = np.frombuffer(data, dtype=np.int16)
+# Main loop
+def init_jarvis():
 
-    # Capture and log warnings
+    try:
+        while True:
+            # Read audio data
+            data = stream.read(CHUNK)
+            # FOR TESTING PURPOSES - Write data to file
+            wf.writeframes(data)
 
-    square = np.square(data_thresh)
-    mean = np.mean(square)
-    sqrt = np.sqrt(mean)
+            # Check if the root mean square is above the threshold
+            rms = np.sqrt(np.mean(np.square(np.frombuffer(data, dtype=np.int16))))
 
-    rms = sqrt
+            if rms > THRESHOLD:
+                wake_word()
 
-    # Check if the root mean square is above the threshold
-    if rms > THRESHOLD and 'hello' in text:
-    # If sound is detected, use Vosk to transcribe speech
-        # If "Hello" is recognized, do something
-        print("Hello detected!")
+    except KeyboardInterrupt:
+        print("Exiting...")
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+if __name__ == "__main__":
+    init_jarvis()
